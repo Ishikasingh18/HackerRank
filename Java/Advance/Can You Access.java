5494762e64ad0a4a7fd21eba78324d73a0837a93
Ishika Singh <91179905+Ishikasingh18@users.noreply.github.com>
2022-08-31 20:59:42 +0530

    Create Can You Access.java

---
+++ Java/Advance/Can You Access.java
@@ -0,0 +1,8 @@
+
+
+o = new Object();
+Solution.Inner instance = new Solution.Inner();
+Inner.Private inner = instance.new Private();
+o=(Object)inner;
+System.out.println(num + " is " + inner.powerof2(num));
+
