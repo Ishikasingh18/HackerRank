99e68eeeea0cef3224016eb42dddf085aa762b6a
Ishika Singh <91179905+Ishikasingh18@users.noreply.github.com>
2022-08-31 20:46:31 +0530

    Create Java Hashset.java

---
+++ Java/Data Structures/Java Hashset.java
@@ -0,0 +1,12 @@
+
+
+//Write your code here
+
+       Set<String> set = new HashSet<String>();
+        for (int i = 0; i < t; i++) {
+            set.add(pair_left[i] + "_" + pair_right[i]);
+            System.out.println(set.size());
+
+        }
+        s.close();
+    
